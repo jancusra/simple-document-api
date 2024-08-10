9c5b455f672a399a70826907c71143701f004f7d
H610I\Rasto <jancusra@gmail.com>
2024-08-10 04:04:43 +0200

    added fixed memory cache unit tests

---
+++ src/Tests/App.Persistence.Tests/MemoryTestsBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace App.Persistence.Tests
+{
+    /// <summary>
+    /// Base class for initializing the tested cache memory
+    /// </summary>
+    public abstract class MemoryTestsBase : IDisposable
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        protected MemoryTestsBase()
+        {
+            var services = new ServiceCollection();
+            services.AddMemoryCache();
+            var serviceProvider = services.BuildServiceProvider();
+
+            _memoryCache = serviceProvider.GetService<IMemoryCache>();
+        }
+
+        public IMemoryCache MemoryCache
+        {
+            get
+            {
+                return _memoryCache;
+            }
+        }
+
+        public void Dispose() { }
+    }
+}
---
+++ src/Tests/App.Persistence.Tests/MemoryUnitTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.Extensions.Caching.Memory;
+using App.Domain.Entities;
+
+namespace App.Persistence.Tests
+{
+    /// <summary>
+    /// Examples of unit tests for an memory cache
+    /// </summary>
+    public class MemoryUnitTests : MemoryTestsBase
+    {
+        [Fact]
+        public void SaveAndGetDocumentFromMemory()
+        {
+            Guid documentId = Guid.NewGuid();
+
+            var document = new Document
+            {
+                Id = documentId,
+                Value = "Some string text value"
+            };
+
+            MemoryCache.Set(documentId, document);
+
+            var result = MemoryCache.Get<Document>(documentId);
+
+            Assert.Equal(result.Id, document.Id);
+            Assert.Equal(result.Value, document.Value);
+        }
+
+        [Fact]
+        public void SaveUpdateAndGetDocumentFromMemory()
+        {
+            Guid documentId = Guid.NewGuid();
+
+            var document = new Document
+            {
+                Id = documentId,
+                Value = "Some new document"
+            };
+
+            MemoryCache.Set(documentId, document);
+
+            document.Value = "Updated document value";
+
+            MemoryCache.Set(documentId, document);
+
+            var result = MemoryCache.Get<Document>(documentId);
+
+            Assert.Equal(result.Id, document.Id);
+            Assert.Equal(result.Value, document.Value);
+        }
+    }
+}
